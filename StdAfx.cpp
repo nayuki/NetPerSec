78768e6a93a8d1c41343caf984c9c715f314a357
Nayuki Minase <nayuki@eigenstate.org>
2013-01-10 14:47:46 +0000

    Manually tweaked StdAfx.cpp, winproc.cpp.

--- StdAfx.cpp
+++ StdAfx.cpp
@@ -1,7 +1,7 @@
 /* 
- * source file that includes just the standard includes
- * netpersec.pch will be the pre-compiled header
- * stdafx.obj will contain the pre-compiled type information
+ * Source file that includes just the standard includes.
+ * netpersec.pch will be the pre-compiled header.
+ * stdafx.obj will contain the pre-compiled type information.
  */
 
 #include "stdafx.h"
--- winproc.cpp
+++ winproc.cpp
@@ -1,9 +1,8 @@
 /* 
- * Collects SNMP statistics and updates the system tray
- * icons.
+ * Collects SNMP statistics and updates the system tray icons.
  */
 
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "NetPerSec.h"
 #include "winproc.h"
 #include "resource.h"
@@ -29,7 +28,6 @@ Cwinproc::Cwinproc() {
 	m_dbSentWrap = 0;
 	m_pPropertiesDlg = 0;
 	ZeroMemory(&m_SystemTray, sizeof(m_SystemTray));
-	
 	ResetData();
 }
 
@@ -54,23 +52,22 @@ BEGIN_MESSAGE_MAP(Cwinproc, CWnd)
 END_MESSAGE_MAP()
 
 
-// Cwinproc message handlers
+/* Cwinproc message handlers */
 
-
-// Startup -- called when the invisible window is created in netpersec.cpp
-// initializes SNMP and the system tray icon
+// Startup - called when the invisible window is created in netpersec.cpp.
+// Initializes SNMP and the system tray icon
 void Cwinproc::StartUp() {
-	if (snmp.Init() == FALSE) {
+	if (snmp.Init() == FALSE)
 		PostQuitMessage(0);
-	} else {
+	else {
 		SetTimer(TIMER_ID_WINPROC, g_nSampleRate, NULL);
 		
-		//1.1 init using NIF_ICON
+		// 1.1 init using NIF_ICON
 		HICON hIcon;
 		if (g_IconStyle == ICON_HISTOGRAM)
-			hIcon = (HICON)LoadImage(AfxGetInstanceHandle(),MAKEINTRESOURCE(IDI_HISTOGRAM),IMAGE_ICON,16,16,LR_DEFAULTCOLOR);
+			hIcon = (HICON)LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDI_HISTOGRAM), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
 		else
-			hIcon = (HICON)LoadImage(AfxGetInstanceHandle(),MAKEINTRESOURCE(IDI_BARGRAPH),IMAGE_ICON,16,16,LR_DEFAULTCOLOR);
+			hIcon = (HICON)LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDI_BARGRAPH), IMAGE_ICON, 16, 16, LR_DEFAULTCOLOR);
 		
 		m_SystemTray.cbSize = sizeof(NOTIFYICONDATA);
 		m_SystemTray.hWnd   = GetSafeHwnd();
@@ -86,35 +83,35 @@ void Cwinproc::StartUp() {
 void Cwinproc::CalcAverages(double dbTotal, DWORD dwTime, DWORD dwBps, STATS_STRUCT *pStats) {
 	ASSERT(g_nAveragingWindow <= MAX_SAMPLES);
 	
-	//set current bps
+	// Set current bps
 	pStats[m_nArrayIndex].Bps = dwBps;
 	
-	//set total bytes sent or recv
+	// Set total bytes sent or recv
 	pStats[m_nArrayIndex].total = dbTotal;
 	
-	//set the current time (milliseconds)
+	// Set the current time (milliseconds)
 	pStats[m_nArrayIndex].time = dwTime;
 	
-	//start is the index in the array for calculating averages
+	// Start is the index in the array for calculating averages
 	int start = m_nArrayIndex - g_nAveragingWindow;
 	if (start < 0)
-		start = MAX_SAMPLES + start;
+		start += MAX_SAMPLES;
 	
-	//the array entry may not have been filled in yet
+	// The array entry may not have been filled in yet
 	if (pStats[start].total == 0)
 		start = 0;
 	
-	//set average based upon sampling window size
+	// Set average based upon sampling window size
 	double dbSampleTotal = 0;
 	
-	//total bytes received/sent in our sampling window
+	// Total bytes received/sent in our sampling window
 	if (pStats[start].total)
 		dbSampleTotal = dbTotal - pStats[start].total;
 	
-	//elapsed milliseconds
-	DWORD dwElapsed = (pStats[m_nArrayIndex].time - pStats[start].time);
+	// Elapsed milliseconds
+	DWORD dwElapsed = pStats[m_nArrayIndex].time - pStats[start].time;
 	
-	//calc average
+	// Calc average
 	if (dwElapsed)
 		pStats[m_nArrayIndex].ave = MulDiv((DWORD)dbSampleTotal, 1000, dwElapsed);
 }
@@ -122,81 +119,76 @@ void Cwinproc::CalcAverages(double dbTotal, DWORD dwTime, DWORD dwBps, STATS_STR
 
 // Calculate samples
 void Cwinproc::OnTimer(UINT /* nIDEvent */) {
-	DWORD s,r, elapsed;
-	DWORD dwRecv_bps, dwSent_bps;
-	DWORD dwTime;
-	double dbRecv, dbSent;
-	
+	DWORD s, r;
 	snmp.GetReceivedAndSentOctets(&r, &s);
 	
-	//init data?
+	// Init data?
 	if (!m_dbTotalBytesRecv || !m_dbTotalBytesSent) {
 		m_dbTotalBytesRecv = r + m_dbRecvWrap;
 		m_dbTotalBytesSent = s + m_dbSentWrap;
 		m_dwStartTime = GetTickCount();
 	}
 	
-	//don't depend upon exact WM_TIMER messages, get the true elapsed number of milliseconds
-	dwTime = GetTickCount();
-	elapsed = (dwTime - m_dwStartTime);
+	// Don't depend upon exact WM_TIMER messages, get the true elapsed number of milliseconds
+	DWORD dwTime = GetTickCount();
+	DWORD elapsed = dwTime - m_dwStartTime;
 	m_dwStartTime = dwTime;
 	
-	dbRecv = r + m_dbRecvWrap;
-	dbSent = s + m_dbSentWrap;
-	
-	//check for integer wrap
+	double dbRecv = r + m_dbRecvWrap;
+	double dbSent = s + m_dbSentWrap;
+	// Check for integer wrap
 	if (dbRecv < m_dbTotalBytesRecv) {
 		m_dbRecvWrap += 0xffffffff;
 		dbRecv = r + m_dbRecvWrap;
 	}
-	
 	if (dbSent < m_dbTotalBytesSent) {
 		m_dbSentWrap += 0xffffffff;
 		dbSent = s + m_dbSentWrap;
 	}
 	
-	//total bytes sent or received during this interval
+	// Total bytes sent or received during this interval
 	DWORD total_recv = (DWORD)(dbRecv - m_dbTotalBytesRecv);
 	DWORD total_sent = (DWORD)(dbSent - m_dbTotalBytesSent);
 	
-	dwRecv_bps = dwSent_bps = 0;
+	DWORD dwRecv_bps = 0;
+	DWORD dwSent_bps = 0;
 	
-	//calc bits per second
+	// Calc bits per second
 	if (elapsed) {
 		dwRecv_bps = MulDiv(total_recv, 1000, elapsed);
 		dwSent_bps = MulDiv(total_sent, 1000, elapsed);
 	}
 	
-	//convert to double
-	double recv_bits = (double)(r);
-	double sent_bits = (double)(s);
+	// Convert to double
+	double recv_bits = (double)r;
+	double sent_bits = (double)s;
 	
-	//calc the average bps
+	// Calc the average bps
 	CalcAverages(recv_bits, dwTime, dwRecv_bps, &RecvStats[0]);
 	CalcAverages(sent_bits, dwTime, dwSent_bps, &SentStats[0]);
 	
-	//get the icon for the system tray
+	// Get the icon for the system tray
 	HICON hIcon = pTheApp->m_Icons.GetIcon(&RecvStats[0], &SentStats[0], m_nArrayIndex, g_IconStyle);
 	UpdateTrayIcon(hIcon);
 	DestroyIcon(hIcon);
 	
-	//increment the circular buffer index
-	if (++m_nArrayIndex >= MAX_SAMPLES)
+	// Increment the circular buffer index
+	m_nArrayIndex++;
+	if (m_nArrayIndex >= MAX_SAMPLES)
 		m_nArrayIndex = 0;
 	
-	//save the totals
+	// Save the totals
 	m_dbTotalBytesRecv = dbRecv;
 	m_dbTotalBytesSent = dbSent;
 }
 
 
-
 void Cwinproc::ShowPropertiesDlg() {
-	if (m_pPropertiesDlg) {
+	if (m_pPropertiesDlg)
 		m_pPropertiesDlg->SetForegroundWindow();
-	} else {
-		//fake out MFC in order to receive the 'minimize all windows' syscommand message
-		//the window is restored in initdialog
+	else {
+		// Fake out MFC in order to receive the "minimize all windows" syscommand message
+		// The window is restored in initdialog
 		pTheApp->m_pMainWnd = NULL;
 		
 		m_pPropertiesDlg = new DlgPropSheet(SZ_APPNAME, NULL);
@@ -223,7 +215,7 @@ LRESULT Cwinproc::OnTaskbarNotify(WPARAM wParam, LPARAM lParam) {
 	switch (lParam) {
 		case WM_MOUSEMOVE:
 		{
-			CString s,sRecvBPS,sRecvAVE;
+			CString s, sRecvBPS, sRecvAVE;
 			int index = GetArrayIndex();
 			
 			FormatBytes(RecvStats[index].Bps, &sRecvBPS);
@@ -253,37 +245,30 @@ LRESULT Cwinproc::OnTaskbarNotify(WPARAM wParam, LPARAM lParam) {
 			menu.LoadMenu(IDR_MENU1);
 			menu.SetDefaultItem(0, TRUE);
 			
-			CMenu *pMenu;
-			pMenu = menu.GetSubMenu(0);
+			CMenu *pMenu = menu.GetSubMenu(0);
 			pMenu->SetDefaultItem(0, TRUE);
 			
-			//see Q135788
+			// See Q135788 "PRB: Menus for Notification Icons Do Not Work Correctly"
 			SetForegroundWindow();
-			int cmd = pMenu->TrackPopupMenu(TPM_RETURNCMD | TPM_LEFTALIGN | TPM_NONOTIFY , pt.x, pt.y,this);
+			int cmd = pMenu->TrackPopupMenu(TPM_RETURNCMD | TPM_LEFTALIGN | TPM_NONOTIFY , pt.x, pt.y, this);
 			PostMessage(WM_NULL, 0, 0);
 			
-			switch (cmd) {
-				case IDCLOSE:
-					//save any settings if the user closes the tray icon while the dlg is open
-					if (m_pPropertiesDlg) {
-						SaveSettings();
-						m_pPropertiesDlg->SendMessage(WM_CLOSE);
-					}
-					
-					pTheApp->m_wnd.PostMessage(WM_CLOSE);
-					break;
-					
-				case ID_PROPERTIES:
-					ShowPropertiesDlg();
-					break;
-			}
+			if (cmd == IDCLOSE) {
+				// Save any settings if the user closes the tray icon while the dialog is open
+				if (m_pPropertiesDlg) {
+					SaveSettings();
+					m_pPropertiesDlg->SendMessage(WM_CLOSE);
+				}
+				pTheApp->m_wnd.PostMessage(WM_CLOSE);
+			} else if (cmd == ID_PROPERTIES)
+				ShowPropertiesDlg();
 		}
 		break;
 	}
 	return 0;
 }
 
-//the index to the array will always be one greater than the actual current value
+// The index to the array will always be one greater than the actual current value
 int Cwinproc::GetArrayIndex() {
 	int i = m_nArrayIndex - 1;
 	if (i < 0)
@@ -305,8 +290,7 @@ void Cwinproc::UpdateTrayIcon(HICON hIcon) {
 }
 
 
-
-// init all arrays
+// Init all arrays
 void Cwinproc::ResetData() {
 	m_nArrayIndex = 0;
 	ZeroMemory(RecvStats, sizeof(RecvStats));
@@ -328,5 +312,5 @@ void Cwinproc::WinHelp(DWORD /*dwData*/, UINT /*nCmd*/) {
 				return;
 		}
 	}
-	CWnd::WinHelp(0,HELP_CONTENTS);
+	CWnd::WinHelp(0, HELP_CONTENTS);
 }
